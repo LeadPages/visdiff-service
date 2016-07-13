c55a2b6aed526891cddd501f8a8df23a4000e46c
benjongbloedt <ben.jongbloedt@ave81.com>
2016-07-13 08:48:40 -0500

    rename complete

--- app/image/v1.py
+++ app/image/v1.py
@@ -3,7 +3,11 @@ from . import image
 from .. import image_diff
 
 
+<<<<<<< 99375249a41b017dfe3a5c7a75a6e87478cab9dd
 @image.route("/api/v1/diff/", methods=['POST'])
+=======
+@image.route("/api/v1/diff/", methods=['GET', 'POST'])
+>>>>>>> rename complete
 def image_diff_endpoint_v1():
     images = request.form.getlist('images')
     if len(images) != 2:
