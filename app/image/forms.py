ee8c3e3b861a9987458258d81e3eeb1ca98733d7
benjongbloedt <ben.jongbloedt@ave81.com>
2016-07-13 11:57:57 -0500

    adding more info

--- app/image/forms.py
+++ app/image/forms.py
@@ -1,14 +1,19 @@
 from flask_wtf import Form
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
-# TODO file allowed
+# TODO gif support?
+images = tuple('jpg jpe jpeg png svg bmp'.split())
+
+
 class ImageSubmitForm(Form):
     image_one = FileField('What is your base image?', validators=[
         FileRequired(),
+        FileAllowed(images, 'Images only!')
     ])
     image_two = FileField('What is the image you want to compare?',
                           validators=[
-                                        FileRequired()
+                                        FileRequired(),
+                                        FileAllowed(images, 'Images only!')
                                     ])
     submit = SubmitField('Submit')
