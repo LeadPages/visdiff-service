8c38f2eb7bb02f3c585f7dc0181477fc513268b2
benjongbloedt <ben.jongbloedt@ave81.com>
2016-07-14 12:18:37 -0500

    diff count and percent done

--- app/image/v2.py
+++ app/image/v2.py
@@ -6,17 +6,17 @@ from .forms import ImageSubmitForm
 from werkzeug.utils import secure_filename
 
 
-@image.route("/api/v1/diff/", methods=['POST'])
-def image_diff_endpoint_v1():
+@image.route("/api/v2/diff/", methods=['POST'])
+def image_diff_endpoint_v2():
     images = request.form.getlist('images')
     if len(images) != 2:
         return jsonify(message="2 images must be specified"), 404
-    return jsonify(image_diff.generate_difference_report(images[0],
-                                                         images[1])), 200
+    return jsonify(image_diff.generate_difference_report_v2(images[0],
+                                                            images[1])), 200
 
 
-@image.route("/v1/diff/", methods=('GET', 'POST'))
-def image_diff_test_page():
+@image.route("/v2/diff/", methods=('GET', 'POST'))
+def image_diff_test_page_v2():
     form = ImageSubmitForm()
     if form.validate_on_submit():
         image_one_name = secure_filename(form.image_one.data.filename)
@@ -27,9 +27,9 @@ def image_diff_test_page():
 
         image_one = base64.b64encode(form.image_one.data.stream.read())
         image_two = base64.b64encode(form.image_two.data.stream.read())
-        diff_report = image_diff.generate_difference_report(image_one,
-                                                            image_two,
-                                                            True)
+        diff_report = image_diff.generate_difference_report_v2(image_one,
+                                                               image_two,
+                                                               True)
         images = [
             dict(name=image_one_name, image=image_one),
             dict(name=image_two_name, image=image_two),
@@ -40,8 +40,3 @@ def image_diff_test_page():
                                report=diff_report,
                                images=images), 200
     return render_template("diff_comparision.html", form=form), 200
-
-
-@image.route("/v1/contains/")
-def image_contains_test_page():
-    return render_template("diff_contains.html"), 200
--- app/image_diff.py
+++ app/image_diff.py
@@ -1,9 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageChops
 import io
 import os
 import math
 import base64
 import cStringIO
+import numpy as np
+
+RGB_BLACK = (0, 0, 0)
+RGBA_BLACK = (0, 0, 0, 0)
+RGB_YELLOW = (128, 128, 0)
 
 
 def generate_difference_report(image_one, image_two,
@@ -85,6 +90,81 @@ def generate_difference_report(image_one, image_two,
     return response
 
 
+def generate_difference_report_v2(image_one, image_two,
+                                  create_diff_file=False):
+    response = {}
+    response['images'] = []
+
+    if is_file(image_one) and is_file(image_two):
+        response['images'].append({'location': image_one})
+        response['images'].append({'location': image_two})
+    else:
+        response['images'].append({'location': 'base64string'})
+        response['images'].append({'location': 'base64string'})
+
+    IMAGE_ONE = load_image(image_one)
+    IMAGE_TWO = load_image(image_two)
+
+    response['images'][0]['size'] = IMAGE_ONE.size
+    response['images'][1]['size'] = IMAGE_TWO.size
+
+    response['outputSize'] = get_output_image_size(IMAGE_ONE, IMAGE_TWO)
+
+    response['images'][0]['mode'] = IMAGE_ONE.mode
+    response['images'][1]['mode'] = IMAGE_TWO.mode
+
+    # iterate through the pixels of both images and compare, if different,
+    # mark the difference image with yellow so it is noticeable to humans
+    diff_count = 0
+    diff_percent = 0.0
+
+    diff_image = Image.fromarray(np.zeros_like(IMAGE_ONE, dtype=np.uint8()), IMAGE_ONE.mode)
+
+
+    diff_arr = np.subtract(np.asarray(IMAGE_ONE, dtype=np.uint8()), np.asarray(IMAGE_TWO, dtype=np.uint8()))
+    diff_count = np.sum(np.any(diff_arr != np.array((0, 0, 0, 0)), axis=2))
+
+    # diff_arr.argwhere
+
+    # for i in range(response['outputSize'][0] - 1):
+    #     for j in range(response['outputSize'][1] - 1):
+    #         if not is_masked(IMAGE_ONE.getpixel((i, j))):
+    #             if pixels_are_different(IMAGE_ONE.getpixel((i, j)),
+    #                                     IMAGE_TWO.getpixel((i, j))):
+    #                 # write a yellow pixel to the difference mask and
+    #                 # increment difference count
+    #                 diff_image.putpixel((i, j), (128, 128, 0))
+
+    diff_percent = (float(diff_count)/(response['outputSize'][0] *
+                                       response['outputSize'][1]))*100
+
+    if create_diff_file:
+        # do not blend if images are found to match because blending
+        # can be expensive
+        if diff_count != 0:
+            # overlay the two input images, then overlay the yellow/black
+            # marked
+            # difference image to generate the output image that a human can
+            # see
+            final_image = Image.blend(IMAGE_ONE, IMAGE_TWO, 0.5)
+            final_image = Image.blend(diff_image, final_image, 0.25)
+            buffer = cStringIO.StringIO()
+            final_image.save(buffer, format="PNG")
+            response['outputImage'] = base64.b64encode(buffer.getvalue())
+        else:
+            final_image = Image.blend(IMAGE_ONE, diff_image, 0.25)
+            buffer = cStringIO.StringIO()
+            final_image.save(buffer, format="PNG")
+            response['outputImage'] = base64.b64encode(buffer.getvalue())
+
+    # it is up to whatever consumes this output to determine whether the
+    # calculated diff percentage or count of different
+    # pixels is acceptible or not
+    response['diffCount'] = diff_count
+    response['diffPercent'] = diff_percent
+    return response
+
+
 def is_file(path):
     """Checks to see if given path is valid"""
     return os.path.exists(os.path.dirname(path))
@@ -127,7 +207,7 @@ def pixels_are_different(pixel1, pixel2):
     pixel2 - tuple(R, G, B) where R/G/B are 0 - 255
     """
     # global DIFF_THRESHOLD
-    DIFF_THRESHOLD = 40
+    DIFF_THRESHOLD = 0
     for component in range(3):
         if math.fabs(pixel1[component] - pixel2[component]) > DIFF_THRESHOLD:
             return True
