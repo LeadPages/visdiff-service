e618af7881e94ff34b58401402d7db22f4a09d95
benjongbloedt <ben.jongbloedt@ave81.com>
2016-07-14 14:15:26 -0500

    splitting up the routes better

--- app/image/v1/routes.py
+++ app/image/v1/routes.py
@@ -2,21 +2,21 @@ from flask import jsonify, request, render_template
 from . import image
 from .. import image_diff
 import base64
-from .forms import ImageSubmitForm
+from ..forms import ImageSubmitForm
 from werkzeug.utils import secure_filename
 
 
-@image.route("/api/v2/diff/", methods=['POST'])
-def image_diff_endpoint_v2():
+@image.route("/api/diff/", methods=['POST'])
+def image_diff_endpoint():
     images = request.form.getlist('images')
     if len(images) != 2:
         return jsonify(message="2 images must be specified"), 404
-    return jsonify(image_diff.generate_difference_report_v2(images[0],
-                                                            images[1])), 200
+    return jsonify(image_diff.generate_difference_report(images[0],
+                                                         images[1])), 200
 
 
-@image.route("/v2/diff/", methods=('GET', 'POST'))
-def image_diff_test_page_v2():
+@image.route("/diff/", methods=('GET', 'POST'))
+def image_diff_test_page():
     form = ImageSubmitForm()
     if form.validate_on_submit():
         image_one_name = secure_filename(form.image_one.data.filename)
@@ -27,9 +27,9 @@ def image_diff_test_page_v2():
 
         image_one = base64.b64encode(form.image_one.data.stream.read())
         image_two = base64.b64encode(form.image_two.data.stream.read())
-        diff_report = image_diff.generate_difference_report_v2(image_one,
-                                                               image_two,
-                                                               True)
+        diff_report = image_diff.generate_difference_report(image_one,
+                                                            image_two,
+                                                            True)
         images = [
             dict(name=image_one_name, image=image_one),
             dict(name=image_two_name, image=image_two),
@@ -40,3 +40,8 @@ def image_diff_test_page_v2():
                                report=diff_report,
                                images=images), 200
     return render_template("diff_comparision.html", form=form), 200
+
+
+@image.route("/contains/")
+def image_contains_test_page():
+    return render_template("diff_contains.html"), 200
